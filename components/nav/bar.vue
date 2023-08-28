bf42eec49c595321a66ce7f78e767c95fcf65852
PichardoWilfred <pichardowilfred@gmail.com>
2023-08-28 16:07:23 -0400

    trying to animate logo

--- components/nav/bar.vue
+++ components/nav/bar.vue
@@ -1,7 +1,7 @@
 <template>
     <header :class="header_class" class="fixed w-full left-0">
-        <nav class="flex justify-center items-center w-full px-8 pb-3">
-            <ul class="navigation flex">
+        <nav class="w-full flex items-center">
+            <ul class="navigation">
                 <li v-for="({ label, class_ }, index) in navigation_bar" :key="index" class="item" :class="class_">
                     {{ label }}
                 </li>
@@ -16,18 +16,24 @@ ul.navigation li.item {
 ul.navigation li.item.focused {
     @apply after:absolute after:rounded-full after:w-[9px] after:h-[9px] after:bg-orange after:top-[-2px] after:right-[-15px];
 }
-
-header.scrolled {
-    @apply top-[-200px];
-}
-header.scrolled.small {
-    @apply top-0;
-}
 header {
-    @apply text-black bg-white top-0 transition-all delay-[50ms];
+    @apply text-black bg-white top-0 transition-all;
 }
 header nav svg.logo {
     @apply my-3 transition-all w-[90px] h-[90px];
+}
+
+header nav {
+    @apply border-b border-b-[transparent];
+}
+header nav ul.navigation {
+    @apply flex items-center px-8 py-10 min-h-[150px] transition-all;
+}
+header.scrolled {
+    @apply top-[-200px];
+}
+header.scrolled.up {
+    @apply top-0;
 }
 header.transparent {
     @apply bg-[#FFFFFF00] text-white;
@@ -35,11 +41,17 @@ header.transparent {
 header.transparent nav svg.logo {
     @apply my-7 w-[115px] h-[115px];
 }
-header.scrolled nav ul.navigation {
-    @apply mt-20;
+header.transparent nav ul.navigation {
+    @apply py-16 px-8;
 }
-header nav ul.navigation {
-    @apply mt-10;
+header.transparent nav {
+    @apply border-b-[transparent];
+}
+header.scrolled nav {
+    @apply border-b-gray-100;
+}
+header.scrolled nav ul.navigation {
+    @apply me-0 py-7;
 }
 </style>
 <script setup>
@@ -48,7 +60,7 @@ import { onMounted, onBeforeUnmount, reactive, ref } from 'vue';
 const header_class = reactive({
     transparent: true,
     scrolled: false,
-    small: false
+    up: false
 })
 const navigation_bar = [
     {
@@ -95,9 +107,9 @@ const handleScroll = () => {
     }
 
     if (currentScrollY < prevScrollY.value && window.scrollY > 800) { // scrolling up
-        header_class.small = true;
+        header_class.up = true;
     }else {
-        header_class.small = false;
+        header_class.up = false;
     }
     prevScrollY.value = currentScrollY;
 }
