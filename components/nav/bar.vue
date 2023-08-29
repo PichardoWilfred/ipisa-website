af9630e4db242990e362d06a01af48caf89efe85
PichardoWilfred <pichardowilfred@gmail.com>
2023-08-28 21:29:41 -0400

    idk

--- components/nav/bar.vue
+++ components/nav/bar.vue
@@ -1,6 +1,9 @@
 <template>
     <header :class="header_class" class="fixed w-full left-0">
-        <nav class="w-full flex items-center">
+        <nav>
+            <div>
+                <ipisa-logo class="logo" />
+            </div>
             <ul class="navigation">
                 <li v-for="({ label, class_ }, index) in navigation_bar" :key="index" class="item" :class="class_">
                     {{ label }}
@@ -11,47 +14,44 @@
 </template>
 <style scoped>
 ul.navigation li.item {
-    @apply relative cursor-pointer font-raleway font-bold me-12 last:me-0;
+    @apply relative cursor-pointer font-raleway font-bold me-10 last:me-0;
 }
 ul.navigation li.item.focused {
-    @apply after:absolute after:rounded-full after:w-[9px] after:h-[9px] after:bg-orange after:top-[-2px] after:right-[-15px];
+    @apply after:absolute after:rounded-full after:w-[9px] after:h-[9px] after:bg-orange-300 after:top-[-2px] after:right-[-15px];
 }
 header {
-    @apply text-black bg-white top-0 transition-all;
+    @apply text-black bg-white top-0 border-b border-b-[transparent] transition-all;
 }
 header nav svg.logo {
-    @apply my-3 transition-all w-[90px] h-[90px];
+    @apply my-4 transition-all w-[90px] h-[90px];
 }
 
 header nav {
-    @apply border-b border-b-[transparent];
+    @apply w-full flex flex-col items-center mb-4 justify-center;
 }
 header nav ul.navigation {
-    @apply flex items-center px-8 py-10 min-h-[150px] transition-all;
+    @apply flex items-center px-8 transition-all;
 }
 header.scrolled {
-    @apply top-[-200px];
+    @apply top-[-200px] border-b-gray-100;
 }
 header.scrolled.up {
     @apply top-0;
 }
 header.transparent {
-    @apply bg-[#FFFFFF00] text-white;
+    @apply bg-[#FFFFFF00] text-white transition-colors;
 }
 header.transparent nav svg.logo {
-    @apply my-7 w-[115px] h-[115px];
+    @apply my-7 w-[130px] h-[130px];
 }
 header.transparent nav ul.navigation {
-    @apply py-16 px-8;
+    @apply px-8;
 }
 header.transparent nav {
     @apply border-b-[transparent];
 }
-header.scrolled nav {
-    @apply border-b-gray-100;
-}
 header.scrolled nav ul.navigation {
-    @apply me-0 py-7;
+    @apply me-0;
 }
 </style>
 <script setup>
@@ -88,29 +88,16 @@ onMounted(() => {
     prevScrollY = ref(window.scrollY);
     window.addEventListener('scroll', handleScroll);
 });
-
 onBeforeUnmount(() => {
     window.removeEventListener('scroll', handleScroll);
 });
 const handleScroll = () => {
     const currentScrollY = window.scrollY;
-    if (window.scrollY > 100) { // scrolling past the limit
-        header_class.transparent = false;
-    }else {
-        header_class.transparent = true;
-    }
 
-    if (window.scrollY > 800) { // scrolling past the limit
-        header_class.scrolled = true;
-    }else {
-        header_class.scrolled = false;
-    }
+    header_class.transparent = window.scrollY < 100;
+    header_class.scrolled = window.scrollY > 800; // scrolling past the limit
+    header_class.up = currentScrollY < prevScrollY.value && window.scrollY > 800; // scrolling up
 
-    if (currentScrollY < prevScrollY.value && window.scrollY > 800) { // scrolling up
-        header_class.up = true;
-    }else {
-        header_class.up = false;
-    }
     prevScrollY.value = currentScrollY;
 }
 </script>
