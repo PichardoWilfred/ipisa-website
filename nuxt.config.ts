36db5c7f9d8e3e8d8a116c4b5008159017100ff0
PichardoWilfred <pichardowilfred@gmail.com>
2023-12-01 16:51:30 -0400

    domain added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,6 +17,18 @@ export default defineNuxtConfig({
       ]
     }
   },
+  image: {
+    screens: {
+      'xs': 320,
+      'sm': 640,
+      'md': 768,
+      'lg': 1024,
+      'xl': 1280,
+      'xxl': 1536,
+      '2xl': 1536
+    },
+    domains: ['https://stellular-nasturtium-aaff86.netlify.app/']
+  },
   modules: [
     '@nuxt/image',
     '@nuxtjs/tailwindcss', // styles
