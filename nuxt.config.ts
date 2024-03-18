d76d844cc7c5559cd20744059923d5d24cd2483f
PichardoWilfred <pichardowilfred@gmail.com>
2024-03-18 17:18:33 -0400

    submodule updated public_html_name

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 const path = require('path');
 export default defineNuxtConfig({nitro: {
   output: {
-    publicDir: path.join(__dirname, './static-build/public')
+    publicDir: path.join(__dirname, './static-build/public_html')
     }
   },
   devtools: { enabled: true },
